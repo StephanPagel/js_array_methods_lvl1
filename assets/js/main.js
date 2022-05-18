5f8acb0ee209817a2a8a4ea254d07486c7fa9335
StephanPagel <stephan.pagel84@gmail.com>
2022-05-18 16:40:25 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,150 @@
+// Lv1_1
+
+// let adresse = ['Hauptstraße', '0815', 'Neustadt', 'Mallorca']; 4
+// let besteFreunde = ['Tick', 'Trick', 'Track'];3
+// let person = ['Donald', 'Duck', 'Donnie', 'Top Secret', 'Quaken', 'Entenhausen', 'Full Stack'];7
+// // console.log(adresse, besteFreunde, person);
+// // person.push(besteFreunde, adresse);
+// // console.log(person);
+// // console.log(person[person.length - 2]);
+// // console.log(person[person.length - 1]);
+
+// Lv1_1_2
+
+// console.log(adresse.length);
+// console.log(besteFreunde.length);
+// console.log(person.length);
+
+// Lv1_2
+
+// let meineTrainer1 = ['Eric', 'Steffen', 'Simon', 'Ahmed'];
+// console.log(meineTrainer1);
+// let meineTrainer2 = new Array('Max', 'Steffen', 'Simon', 'Ahmed');
+// console.log(meineTrainer2);
+// let meineTrainer3 = new Array();
+// meineTrainer3[0] = 'Eric';
+// meineTrainer3[1] = 'Steffen';
+// meineTrainer3[2] = 'Simon';
+// meineTrainer3[3] = 'Ahmed';
+// console.log(meineTrainer3);
+
+// Lv1_3
+
+// let numberArray = [5, 6, 7, 8, 9, 10];
+// console.log(numberArray);
+// console.log(numberArray[0]);
+// console.log(numberArray[1]);
+// console.log(numberArray[2]);
+// console.log(numberArray[3]);
+// console.log(numberArray[4]);
+// console.log(numberArray[5]);
+
+// Lv1_4
+
+// let meinText1 = 'Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag.';
+// let meinText2 = 'Wie geht es dir heute?';
+// let meinText3 = 'Heute ist ein großer Tag für uns.';
+
+// let split1 = meinText1.split();
+// let split2 = meinText1.split('');
+// let split3 = meinText1.split(' ');
+// let split4 = meinText2.split();
+// let split5 = meinText2.split('');
+// let split6 = meinText2.split(' ');
+// let split7 = meinText3.split();
+// let split8 = meinText3.split('');
+// let split9 = meinText3.split(' ');
+// console.log(split1);
+// console.log(split2);
+// console.log(split3);
+// console.log(split4);
+// console.log(split5);
+// console.log(split6);
+// console.log(split7);
+// console.log(split8);
+// console.log(split9);
+
+//Lv 1_5_1
+
+// let songs = ['Bohemian Rhapsody', 'Stairway to heaven', 'Hotel California'];
+// let totalSongs = songs.push("Sweet Child O'Mine", "Back to school", "Galvanize");
+// console.log(songs);
+
+// let besteFussballerAllerZeiten = ['Messi', 'Ronaldo', 'Ronaldinho', 'Müller', 'Kroos'];
+// besteFussballerAllerZeiten.push('Manuel', 'Klaus', 'Heinz');
+// console.log(besteFussballerAllerZeiten);
+
+//Lv 1_5_2
+
+// let heroAndEnemy = [['Batman', 'The Joker'], ['Professor X', ' Magneto'], ['Thor', 'Loki']];
+// heroAndEnemy.push(['Chip', 'Chap'], ['Bibi', 'Tina'], ['Tom', 'Jerry']);
+// console.log(heroAndEnemy);
+
+//Lv 1_6
+
+// let entfernterSong = songs.pop();
+// console.log(entfernterSong, songs);
+
+// let entfernterFußballer = besteFussballerAllerZeiten.pop();
+// console.log(entfernterFußballer, besteFussballerAllerZeiten);
+
+//Lv 1_7
+
+// let deutscheGerichte = ['Speckkuchen', 'Thüringer Rostbratwurst', 'Quarkkeulchen', 'Sauerbraten'];
+// deutscheGerichte.unshift('Tote Oma', 'Eisbein', 'Eintopf', 'Kartoffelsalat', 'Würstchen');
+// console.log(deutscheGerichte);
+
+// //Lv 1_8
+
+// let nichtGut = [deutscheGerichte.shift(), deutscheGerichte.shift(), deutscheGerichte.shift()];
+// console.log(nichtGut);
+
+//Lv 1_9
+
+// let numbers = [23, 54, 75];
+// numbers.push(99, 76, 21);
+// numbers.unshift(11, 22, 33, 44, 55);
+// console.log(numbers.pop());
+// console.log(numbers.pop());
+// numbers.shift();
+// numbers.shift();
+// console.log(numbers);
+
+//Lv 1_12
+
+// let array = [
+//     "imageTeilnehmer000supercode.jpg",
+//     "imageTeilnehmer001supercode.jpg",
+//     "imageTeilnehmer002supercode.jpg",
+//     "imageTeilnehmer003supercode.jpg",
+//     "imageTeilnehmer004supercode.jpg",
+//     "imageTeilnehmer005supercode.jpg",
+//     "imageTeilnehmer006supercode.jpg",
+//     "imageTeilnehmer007supercode.jpg",
+//     "imageTeilnehmer008supercode.jpg",
+//     "imageTeilnehmer009supercode.jpg",
+//     "imageTeilnehmer010supercode.jpg",
+//     "imageTeilnehmer011supercode.jpg",
+//     "imageTeilnehmer012supercode.jpg",
+//     "imageTeilnehmer013supercode.jpg",
+//     "imageTeilnehmer014supercode.jpg",
+//     "imageTeilnehmer015supercode.jpg",
+//     "imageTeilnehmer016supercode.jpg",
+//     "imageTeilnehmer017supercode.jpg",
+//     "imageTeilnehmer018supercode.jpg",
+//     "imageTeilnehmer019supercode.jpg"
+// ];
+
+// let copyImg1 = array.slice(7, 15);
+// console.log(copyImg1);
+// console.log(array);
+
+// let copyImg2 = array.slice(6, 12);
+// console.log(copyImg2);
+// console.log(array);
+
+
+
+
+
+
